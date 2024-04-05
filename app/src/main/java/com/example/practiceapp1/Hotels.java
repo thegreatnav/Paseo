a576adcd873cb830215247fac77b198623ddaf74
Navneeth <navneethsanthosh03@gmail.com>
2024-04-05 13:13:35 +0530

    commit2

---
+++ app/src/main/java/com/example/practiceapp1/Hotels.java
@@ -0,0 +1,53 @@
+package com.example.practiceapp1;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.core.view.WindowCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.practiceapp1.databinding.ActivityHotelsBinding;
+
+public class Hotels extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityHotelsBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityHotelsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_hotels);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAnchorView(R.id.fab)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_hotels);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+}
--- app/src/main/java/com/example/practiceapp1/MainActivity.java
+++ app/src/main/java/com/example/practiceapp1/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.practiceapp1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
+import android.widget.*;
 
+import com.example.practiceapp1.RecentsAdapter;
+import com.example.practiceapp1.TopPlacesAdapter;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
@@ -13,53 +17,101 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.practiceapp1.databinding.ActivityMainBinding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration mAppBarConfiguration;
-    private ActivityMainBinding binding;
+    RecyclerView recentRecycler, topPlacesRecycler;
+    RecentsAdapter recentsAdapter;
+    TopPlacesAdapter topPlacesAdapter;
+    TextView tv4;
+    ImageView hotels,flights,profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recentRecycler = findViewById(R.id.recent_recycler);
+        topPlacesRecycler = findViewById(R.id.top_places_recycler);
+        tv4=findViewById(R.id.textView4);
+        hotels=findViewById(R.id.button_hotels);
+        flights=findViewById(R.id.button_flights);
+        profile=findViewById(R.id.button_profile);
+        List<RecentsData> recentsDataList = new ArrayList<>();
+        recentsDataList.add(new RecentsData("AM Lake","India","From $200",R.drawable.recentimage1));
+        recentsDataList.add(new RecentsData("Nilgiri Hills","India","From $300",R.drawable.recentimage2));
+        recentsDataList.add(new RecentsData("AM Lake","India","From $200",R.drawable.recentimage1));
+        recentsDataList.add(new RecentsData("Nilgiri Hills","India","From $300",R.drawable.recentimage2));
+        recentsDataList.add(new RecentsData("AM Lake","India","From $200",R.drawable.recentimage1));
+        recentsDataList.add(new RecentsData("Nilgiri Hills","India","From $300",R.drawable.recentimage2));
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setRecentRecycler(recentsDataList);
 
-        setSupportActionBar(binding.appBarMain.toolbar);
-        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
+        List<TopPlacesData> topPlacesDataList = new ArrayList<>();
+        topPlacesDataList.add(new TopPlacesData("Kasimir Hill","India","$200 - $500",R.drawable.topplaces));
+        topPlacesDataList.add(new TopPlacesData("Kasimir Hill","India","$200 - $500",R.drawable.topplaces));
+        topPlacesDataList.add(new TopPlacesData("Kasimir Hill","India","$200 - $500",R.drawable.topplaces));
+        topPlacesDataList.add(new TopPlacesData("Kasimir Hill","India","$200 - $500",R.drawable.topplaces));
+        topPlacesDataList.add(new TopPlacesData("Kasimir Hill","India","$200 - $500",R.drawable.topplaces));
+
+        setTopPlacesRecycler(topPlacesDataList);
+
+        tv4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with my own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent i=new Intent(MainActivity.this, DetailsActivity.class);
+                startActivity(i);
             }
         });
-        DrawerLayout drawer = binding.drawerLayout;
-        NavigationView navigationView = binding.navView;
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
-                .setOpenableLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+
+        hotels.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(MainActivity.this, Hotels.class);
+                startActivity(i);
+            }
+        });
+
+        flights.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(MainActivity.this, Flights.class);
+                startActivity(i);
+            }
+        });
+
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(MainActivity.this, ProfileActivity.class);
+                startActivity(i);
+            }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    private  void setRecentRecycler(List<RecentsData> recentsDataList){
+
+        recentRecycler = findViewById(R.id.recent_recycler);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false);
+        recentRecycler.setLayoutManager(layoutManager);
+        recentsAdapter = new RecentsAdapter(this, recentsDataList);
+        recentRecycler.setAdapter(recentsAdapter);
+
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
+    private  void setTopPlacesRecycler(List<TopPlacesData> topPlacesDataList){
+
+        topPlacesRecycler = findViewById(R.id.top_places_recycler);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.VERTICAL, false);
+        topPlacesRecycler.setLayoutManager(layoutManager);
+        topPlacesAdapter = new TopPlacesAdapter(this, topPlacesDataList);
+        topPlacesRecycler.setAdapter(topPlacesAdapter);
+
     }
 }
