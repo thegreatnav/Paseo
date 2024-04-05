6da67db8df5b20142df0586bbc6586aea2629915
Navneeth <navneethsanthosh03@gmail.com>
2024-04-05 15:25:07 +0530

    commit4

--- app/src/main/java/com/example/practiceapp1/Flights.java
+++ app/src/main/java/com/example/practiceapp1/Flights.java
@@ -1,24 +1,13 @@
 package com.example.practiceapp1;
 
 import android.os.Bundle;
-
 import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.view.View;
-
-import androidx.core.view.WindowCompat;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
 import com.example.practiceapp1.databinding.ActivityFlightsBinding;
 
 public class Flights extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
     private ActivityFlightsBinding binding;
 
     @Override
@@ -30,10 +19,6 @@ public class Flights extends AppCompatActivity {
 
         setSupportActionBar(binding.toolbar);
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_flights);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -42,12 +27,5 @@ public class Flights extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_flights);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
     }
 }
--- app/src/main/java/com/example/practiceapp1/Hotels.java
+++ app/src/main/java/com/example/practiceapp1/Hotels.java
@@ -1,24 +1,13 @@
 package com.example.practiceapp1;
 
 import android.os.Bundle;
-
 import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.view.View;
-
-import androidx.core.view.WindowCompat;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
 import com.example.practiceapp1.databinding.ActivityHotelsBinding;
 
 public class Hotels extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
     private ActivityHotelsBinding binding;
 
     @Override
@@ -30,10 +19,6 @@ public class Hotels extends AppCompatActivity {
 
         setSupportActionBar(binding.toolbar);
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_hotels);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -42,12 +27,5 @@ public class Hotels extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_hotels);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
     }
 }
